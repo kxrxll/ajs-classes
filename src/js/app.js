9cd316e59c317acf5c7a2489c45c9e1b640a55f7
kxrxll <kovalyovkirill@gmail.com>
2021-08-09 14:34:57 +0300

    classes created

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
-// TODO: write your code here
-import sum from './basic';
+import Character from './character';
 
-console.log('worked');
+import Bowman from './bowman';
+import Swordsman from './swordsman';
+import Magician from './magician';
+import Undead from './undead';
+import Zombie from './zombie';
+import Daemon from './daemon';
 
-console.log(sum([1, 2]));
+const character = new Character('Василий', 'Swordsman', 80, 5, 100, 70);
+
+const bowman = new Bowman('Леголас');
+const swordsman = new Swordsman('Баромир');
+const magician = new Magician('Гендальф');
+const undead = new Undead('Сильвана');
+const zombie = new Zombie('Гуль');
+const daemon = new Daemon('Саргерас');
+
+// eslint-disable-next-line no-console
+console.log(character, bowman, swordsman, magician, undead, zombie, daemon);
